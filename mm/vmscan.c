e82a48a01dc5fb76df5467062daceeacc0ea7252
Yafang Shao <laoar.shao@gmail.com>
2019-12-11 14:24:22 +1100

    mm/vmscan: expose cgroup_ino for memcg reclaim tracepoints
    We can use the exposed cgroup_ino to trace specified cgroup.
    
    For example,
    step 1, get the inode of the specified cgroup
    	$ ls -di /tmp/cgroupv2/foo
    step 2, set this inode into tracepoint filter to trace this cgroup only
    	(assume the inode is 11)
    	$ cd /sys/kernel/debug/tracing/events/vmscan/
    	$ echo 'cgroup_ino == 11' > mm_vmscan_memcg_reclaim_begin/filter
    	$ echo 'cgroup_ino == 11' > mm_vmscan_memcg_reclaim_end/filter
    
    The reason I made this change is to trace a specific container.
    
    Sometimes there're lots of containers on one host.  Some of them are
    not important at all, so we don't care whether them are under memory
    pressure.  While some of them are important, so we want't to know if
    these containers are doing memcg reclaim and how long this relaim
    takes.
    
    Without this change, we don't know the memcg reclaim happend in which
    container.
    
    Link: http://lkml.kernel.org/r/1557649528-11676-1-git-send-email-laoar.shao@gmail.com
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: <shaoyafang@didiglobal.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3316,8 +3316,10 @@ unsigned long mem_cgroup_shrink_node(struct mem_cgroup *memcg,
 	sc.gfp_mask = (gfp_mask & GFP_RECLAIM_MASK) |
 			(GFP_HIGHUSER_MOVABLE & ~GFP_RECLAIM_MASK);
 
-	trace_mm_vmscan_memcg_softlimit_reclaim_begin(sc.order,
-						      sc.gfp_mask);
+	trace_mm_vmscan_memcg_softlimit_reclaim_begin(
+					cgroup_ino(memcg->css.cgroup),
+					sc.order,
+					sc.gfp_mask);
 
 	/*
 	 * NOTE: Although we can get the priority field, using it
@@ -3328,7 +3330,9 @@ unsigned long mem_cgroup_shrink_node(struct mem_cgroup *memcg,
 	 */
 	shrink_lruvec(lruvec, &sc);
 
-	trace_mm_vmscan_memcg_softlimit_reclaim_end(sc.nr_reclaimed);
+	trace_mm_vmscan_memcg_softlimit_reclaim_end(
+					cgroup_ino(memcg->css.cgroup),
+					sc.nr_reclaimed);
 
 	*nr_scanned = sc.nr_scanned;
 
@@ -3363,7 +3367,9 @@ unsigned long try_to_free_mem_cgroup_pages(struct mem_cgroup *memcg,
 
 	set_task_reclaim_state(current, &sc.reclaim_state);
 
-	trace_mm_vmscan_memcg_reclaim_begin(0, sc.gfp_mask);
+	trace_mm_vmscan_memcg_reclaim_begin(
+				cgroup_ino(memcg->css.cgroup),
+				0, sc.gfp_mask);
 
 	psi_memstall_enter(&pflags);
 	noreclaim_flag = memalloc_noreclaim_save();
@@ -3373,7 +3379,9 @@ unsigned long try_to_free_mem_cgroup_pages(struct mem_cgroup *memcg,
 	memalloc_noreclaim_restore(noreclaim_flag);
 	psi_memstall_leave(&pflags);
 
-	trace_mm_vmscan_memcg_reclaim_end(nr_reclaimed);
+	trace_mm_vmscan_memcg_reclaim_end(
+				cgroup_ino(memcg->css.cgroup),
+				nr_reclaimed);
 	set_task_reclaim_state(current, NULL);
 
 	return nr_reclaimed;
