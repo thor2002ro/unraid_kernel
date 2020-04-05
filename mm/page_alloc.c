67a9f07b11c0f2952262a2604ac0b21b1610766c
Roman Gushchin <guro@fb.com>
2020-04-06 09:53:47 +1000

    mmpage_alloccma-conditionally-prefer-cma-pageblocks-for-movable-allocations-fix
    ifdef the cma-specific code.
    
    Link: http://lkml.kernel.org/r/20200311225832.GA178154@carbon.DHCP.thefacebook.com
    Cc: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Qian Cai <cai@lca.pw>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Joonsoo Kim <js1304@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2759,6 +2759,7 @@ __rmqueue(struct zone *zone, unsigned int order, int migratetype,
 {
 	struct page *page;
 
+#ifdef CONFIG_CMA
 	/*
 	 * Balance movable allocations between regular and CMA areas by
 	 * allocating from CMA when over half of the zone's free memory
@@ -2771,6 +2772,7 @@ __rmqueue(struct zone *zone, unsigned int order, int migratetype,
 		if (page)
 			return page;
 	}
+#endif
 retry:
 	page = __rmqueue_smallest(zone, order, migratetype);
 	if (unlikely(!page)) {
