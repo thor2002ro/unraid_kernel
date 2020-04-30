146e676b4c2935b565a11da6cd27b4a67ab0a0c4
Minchan Kim <minchan@kernel.org>
2020-04-30 16:18:19 +1000

    fix process_madvise build break for arm64
    0-day reported build break from process_madvise on ARM64.
    
       aarch64-linux-ld: arch/arm64/kernel/head.o: relocation R_AARCH64_ABS32 against `_kernel_offset_le_lo32' can not be used when making a shared object
       aarch64-linux-ld: arch/arm64/kernel/efi-entry.stub.o: relocation R_AARCH64_ABS32 against `__efistub_stext_offset' can not be used when making a shared object
       arch/arm64/kernel/head.o: In function `kimage_vaddr':
       (.idmap.text+0x0): dangerous relocation: unsupported relocation
       arch/arm64/kernel/head.o: In function `__primary_switch':
       (.idmap.text+0x378): dangerous relocation: unsupported relocation
       (.idmap.text+0x380): dangerous relocation: unsupported relocation
    >> arch/arm64/kernel/sys32.o:(.rodata+0xdb8): undefined reference to `__arm64_process_madvise'
    
    This patch should fix it.
    
    Link: http://lkml.kernel.org/r/20200303145756.GA219683@google.com
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -884,7 +884,7 @@ __SYSCALL(__NR_openat2, sys_openat2)
 #define __NR_pidfd_getfd 438
 __SYSCALL(__NR_pidfd_getfd, sys_pidfd_getfd)
 #define __NR_process_madvise 439
-__SYSCALL(__NR_process_madvise, process_madvise)
+__SYSCALL(__NR_process_madvise, sys_process_madvise)
 
 /*
  * Please add new compat syscalls above this comment and update
