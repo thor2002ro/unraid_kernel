92d727916d47d93837d3981c81da2bcb3272c194
thor2002ro <thor2002ro@yahoo.com>
2020-11-03 16:51:21 +0200

    fix unraid md spindown/spinup v2

--- drivers/md/unraid.c
+++ drivers/md/unraid.c
@@ -2095,14 +2095,3 @@ int unraid_dump(mddev_t *mddev)
 
 	return 0;
 }
-
-int ioctl_by_bdev(struct block_device *bdev, unsigned cmd, unsigned long arg)
-{
-	int res;
-	mm_segment_t old_fs = get_fs();
-	set_fs(KERNEL_DS);
-	res = blkdev_ioctl(bdev, 0, cmd, arg);
-	set_fs(old_fs);
-	return res;
-}
-
