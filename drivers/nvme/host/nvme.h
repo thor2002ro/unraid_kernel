3acc7821d010586c69aba7c0f827df5484528014
Keith Busch <kbusch@kernel.org>
2021-06-10 14:44:37 -0700

    nvme: use return value from blk_execute_rq()
    We don't have an nvme status to report if the driver's .queue_rq()
    returns an error without dispatching the requested nvme command. Check
    the return value from blk_execute_rq() for all passthrough commands so
    the caller may know their command was not successful.
    
    If the command is from the target passthrough interface and fails to
    dispatch, synthesize the response back to the host as a internal target
    error.
    
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Link: https://lore.kernel.org/r/20210610214437.641245-5-kbusch@kernel.org
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/nvme/host/nvme.h
+++ drivers/nvme/host/nvme.h
@@ -871,7 +871,7 @@ static inline void nvme_hwmon_exit(struct nvme_ctrl *ctrl)
 
 u32 nvme_command_effects(struct nvme_ctrl *ctrl, struct nvme_ns *ns,
 			 u8 opcode);
-void nvme_execute_passthru_rq(struct request *rq);
+int nvme_execute_passthru_rq(struct request *rq);
 struct nvme_ctrl *nvme_ctrl_from_file(struct file *file);
 struct nvme_ns *nvme_find_get_ns(struct nvme_ctrl *ctrl, unsigned nsid);
 void nvme_put_ns(struct nvme_ns *ns);
