5682074a5535f417776d1ea7df8d0a5a82092bc0
thor2002ro <thor2002ro@yahoo.com>
2021-06-17 13:42:15 +0300

    fix R8156 2.5G Ethernet adapters probe

--- drivers/net/usb/cdc_ether.c
+++ drivers/net/usb/cdc_ether.c
@@ -748,6 +748,13 @@ static const struct usb_device_id	products[] = {
 	.driver_info = 0,
 },
 
+/* Realtek RTL8156 Based USB 3.0 Ethernet Adapters */
+{
+	USB_DEVICE_AND_INTERFACE_INFO(REALTEK_VENDOR_ID, 0x8156, USB_CLASS_COMM,
+			USB_CDC_SUBCLASS_ETHERNET, USB_CDC_PROTO_NONE),
+	.driver_info = 0,
+},
+
 /* Samsung USB Ethernet Adapters */
 {
 	USB_DEVICE_AND_INTERFACE_INFO(SAMSUNG_VENDOR_ID, 0xa101, USB_CLASS_COMM,
