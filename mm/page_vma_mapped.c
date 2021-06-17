b776cd6d30093fd00773cf283cfede899b9d615e
Hugh Dickins <hughd@google.com>
2021-06-17 15:16:32 +1000

    mm: page_vma_mapped_walk(): add a level of indentation fix
    add a tab to the READ_ONCE line in the indentation patch.
    
    Link: https://lkml.kernel.org/r/7f817555-3ce1-c785-e438-87d8efdcaf26@google.com
    Signed-off-by: Hugh Dickins <hughd@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/page_vma_mapped.c
+++ mm/page_vma_mapped.c
@@ -190,7 +190,7 @@ restart:
 		 * compiler and used as a stale value after we've observed a
 		 * subsequent update.
 		 */
-	pmde = READ_ONCE(*pvmw->pmd);
+		pmde = READ_ONCE(*pvmw->pmd);
 
 		if (pmd_trans_huge(pmde) || is_pmd_migration_entry(pmde)) {
 			pvmw->ptl = pmd_lock(mm, pvmw->pmd);
