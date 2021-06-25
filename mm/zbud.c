b2bbdedd886cab2d50b3aeb98bf448ac9a973c61
Miaohe Lin <linmiaohe@huawei.com>
2021-06-25 14:36:39 +1000

    mm/zbud: fix unused function warnings when CONFIG_ZPOOL is disabled
    There are several -Wunused-function warnings when CONFIG_ZPOOL is
    disabled.  As zbud won't do anything when CONFIG_ZPOOL is disabled, we can
    make zbud depends on ZPOOL and eliminate the CONFIG_ZPOOL macro in zbud.c
    to fix these warnings.
    
    Link: https://lkml.kernel.org/r/20210619025508.1239386-1-linmiaohe@huawei.com
    Fixes: 962b7e9ad124 ("mm/zbud: don't export any zbud API")
    Reported-by: Nathan Chancellor <nathan@kernel.org>
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Dan Streetman <ddstreet@ieee.org>
    Cc: Seth Jennings <sjenning@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/zbud.c
+++ mm/zbud.c
@@ -109,10 +109,8 @@ struct zbud_pool {
 	struct list_head lru;
 	u64 pages_nr;
 	const struct zbud_ops *ops;
-#ifdef CONFIG_ZPOOL
 	struct zpool *zpool;
 	const struct zpool_ops *zpool_ops;
-#endif
 };
 
 /*
@@ -524,8 +522,6 @@ static u64 zbud_get_pool_size(struct zbud_pool *pool)
  * zpool
  ****************/
 
-#ifdef CONFIG_ZPOOL
-
 static int zbud_zpool_evict(struct zbud_pool *pool, unsigned long handle)
 {
 	if (pool->zpool && pool->zpool_ops && pool->zpool_ops->evict)
@@ -616,7 +612,6 @@ static struct zpool_driver zbud_zpool_driver = {
 };
 
 MODULE_ALIAS("zpool-zbud");
-#endif /* CONFIG_ZPOOL */
 
 static int __init init_zbud(void)
 {
@@ -624,19 +619,14 @@ static int __init init_zbud(void)
 	BUILD_BUG_ON(sizeof(struct zbud_header) > ZHDR_SIZE_ALIGNED);
 	pr_info("loaded\n");
 
-#ifdef CONFIG_ZPOOL
 	zpool_register_driver(&zbud_zpool_driver);
-#endif
 
 	return 0;
 }
 
 static void __exit exit_zbud(void)
 {
-#ifdef CONFIG_ZPOOL
 	zpool_unregister_driver(&zbud_zpool_driver);
-#endif
-
 	pr_info("unloaded\n");
 }
 
