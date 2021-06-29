fb38ab9c38c97db7ac5711704e41450212ab231d
Maor Gottlieb <maorg@nvidia.com>
2021-06-29 11:40:02 +0300

    RDMA: Use dma_map_sgtable for map umem pages
    In order to avoid incorrect usage of sg_table fields, change umem to
    use dma_map_sgtable for map the pages for DMA. Since dma_map_sgtable
    update the nents field (number of DMA entries), there is no need
    anymore for nmap variable, hence do some cleanups accordingly.
    
    Link: https://lore.kernel.org/r/70cbe6ddc2aa9bc5efb96d3c932d76fb2d68a50c.1624955710.git.leonro@nvidia.com
    Signed-off-by: Maor Gottlieb <maorg@nvidia.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/infiniband/core/umem_dmabuf.c
+++ drivers/infiniband/core/umem_dmabuf.c
@@ -57,7 +57,6 @@ int ib_umem_dmabuf_map_pages(struct ib_umem_dmabuf *umem_dmabuf)
 
 	umem_dmabuf->umem.sg_head.sgl = umem_dmabuf->first_sg;
 	umem_dmabuf->umem.sg_head.nents = nmap;
-	umem_dmabuf->umem.nmap = nmap;
 	umem_dmabuf->sgt = sgt;
 
 wait_fence:
