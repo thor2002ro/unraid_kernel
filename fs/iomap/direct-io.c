59fa128c9d56c023492c9d7650399808c76ddf17
Andreas Gruenbacher <agruenba@redhat.com>
2021-07-24 12:26:41 +0200

    iomap: Add done_before argument to iomap_dio_rw
    Add a done_before argument to iomap_dio_rw that indicates how much of
    the request has already been transferred.  When the request succeeds, we
    report that done_before additional bytes were tranferred.  This is
    useful for finishing a request asynchronously when part of the request
    has already been completed synchronously.
    
    We'll use that to allow iomap_dio_rw to be used with page faults
    disabled: when a page fault occurs while submitting a request, we
    synchronously complete the part of the request that has already been
    submitted.  The caller can then take care of the page fault and call
    iomap_dio_rw again for the rest of the request, passing in the number of
    bytes already tranferred.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/iomap/direct-io.c
+++ fs/iomap/direct-io.c
@@ -31,6 +31,7 @@ struct iomap_dio {
 	atomic_t		ref;
 	unsigned		flags;
 	int			error;
+	size_t			done_before;
 	bool			wait_for_completion;
 
 	union {
@@ -123,6 +124,9 @@ ssize_t iomap_dio_complete(struct iomap_dio *dio)
 	 */
 	if (ret > 0 && (dio->flags & IOMAP_DIO_NEED_SYNC))
 		ret = generic_write_sync(iocb, ret);
+
+	if (ret > 0)
+		ret += dio->done_before;
 
 	kfree(dio);
 
@@ -456,7 +460,7 @@ static loff_t iomap_dio_iter(const struct iomap_iter *iter,
 struct iomap_dio *
 __iomap_dio_rw(struct kiocb *iocb, struct iov_iter *iter,
 		const struct iomap_ops *ops, const struct iomap_dio_ops *dops,
-		unsigned int dio_flags)
+		unsigned int dio_flags, size_t done_before)
 {
 	struct address_space *mapping = iocb->ki_filp->f_mapping;
 	struct inode *inode = file_inode(iocb->ki_filp);
@@ -486,6 +490,7 @@ __iomap_dio_rw(struct kiocb *iocb, struct iov_iter *iter,
 	dio->dops = dops;
 	dio->error = 0;
 	dio->flags = 0;
+	dio->done_before = done_before;
 
 	dio->submit.iter = iter;
 	dio->submit.waiter = current;
@@ -652,11 +657,11 @@ EXPORT_SYMBOL_GPL(__iomap_dio_rw);
 ssize_t
 iomap_dio_rw(struct kiocb *iocb, struct iov_iter *iter,
 		const struct iomap_ops *ops, const struct iomap_dio_ops *dops,
-		unsigned int dio_flags)
+		unsigned int dio_flags, size_t done_before)
 {
 	struct iomap_dio *dio;
 
-	dio = __iomap_dio_rw(iocb, iter, ops, dops, dio_flags);
+	dio = __iomap_dio_rw(iocb, iter, ops, dops, dio_flags, done_before);
 	if (IS_ERR_OR_NULL(dio))
 		return PTR_ERR_OR_ZERO(dio);
 	return iomap_dio_complete(dio);
