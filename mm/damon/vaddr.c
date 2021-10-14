1e1c5ac4311e2a850d2185db8476e47646c96d6b
SeongJae Park <sj@kernel.org>
2021-10-14 11:08:48 +0000

    mm/damon/vaddr: Include 'highmem.h' to fix a build failure
    Commit 0ff28922686c ("mm/damon/vaddr: separate commonly usable
    functions") in -mm tree[1] moves include of 'highmem.h' from 'vaddr.c'
    to 'prmtv-common.c', though the code for the header is still in
    'vaddr.c'.  As a result, build with 'CONFIG_HIGHPTE' fails as below:
    
        In file included from ../include/linux/mm.h:33:0,
                          from ../include/linux/kallsyms.h:13,
                          from ../include/linux/bpf.h:20,
                          from ../include/linux/bpf-cgroup.h:5,
                          from ../include/linux/cgroup-defs.h:22,
                          from ../include/linux/cgroup.h:28,
                          from ../include/linux/hugetlb.h:9,
                          from ../mm/damon/vaddr.c:11:
        ../mm/damon/vaddr.c: In function ‘damon_mkold_pmd_entry’:
        ../include/linux/pgtable.h:97:12: error: implicit declaration of function ‘kmap_atomic’; did you mean ‘mcopy_atomic’? [-Werror=implicit-function-declaration]
           ((pte_t *)kmap_atomic(pmd_page(*(dir))) +  \
                     ^
        ../include/linux/mm.h:2376:17: note: in expansion of macro ‘pte_offset_map’
           pte_t *__pte = pte_offset_map(pmd, address); \
                          ^~~~~~~~~~~~~~
        ../mm/damon/vaddr.c:387:8: note: in expansion of macro ‘pte_offset_map_lock’
           pte = pte_offset_map_lock(walk->mm, pmd, addr, &ptl);
                 ^~~~~~~~~~~~~~~~~~~
        ../include/linux/pgtable.h:99:24: error: implicit declaration of function ‘kunmap_atomic’; did you mean ‘in_atomic’? [-Werror=implicit-function-declaration]
          #define pte_unmap(pte) kunmap_atomic((pte))
                                 ^
        ../include/linux/mm.h:2384:2: note: in expansion of macro ‘pte_unmap’
           pte_unmap(pte);     \
           ^~~~~~~~~
        ../mm/damon/vaddr.c:392:2: note: in expansion of macro ‘pte_unmap_unlock’
           pte_unmap_unlock(pte, ptl);
           ^~~~~~~~~~~~~~~~
    
    This commit fixes the issue by moving the include back to 'vaddr.c'.
    
    [1] https://github.com/hnaz/linux-mm/commit/0ff28922686c
    
    Reported-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/damon/vaddr.c
+++ mm/damon/vaddr.c
@@ -8,6 +8,7 @@
 #define pr_fmt(fmt) "damon-va: " fmt
 
 #include <asm-generic/mman-common.h>
+#include <linux/highmem.h>
 #include <linux/hugetlb.h>
 #include <linux/mmu_notifier.h>
 #include <linux/page_idle.h>
