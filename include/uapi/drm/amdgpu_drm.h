c8d265840be604ebaa66a426913ecd4adb619311
Alex Deucher <alexander.deucher@amd.com>
2021-11-23 14:54:17 -0500

    drm/amdgpu/UAPI: add new CTX OP for setting profile modes
    Add a new CTX ioctl operation to set profile modes.  When
    creating traces for tools like RGP or using SPM or doing
    performance profiling, it's required to enable a special
    stable profiling power state on the GPU.  These profiling
    states set fixed clocks and disable certain other power
    features like powergating which may impact the results.
    
    Historically, these profiles were enabled via sysfs, but
    this adds an interface to enable it via the CTX ioctl
    from the application.  Since the power state is global
    only one application can set it at a time, so if multiple
    applications try and use it only the first will get it,
    the ioctl will return -EBUSY for others.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- include/uapi/drm/amdgpu_drm.h
+++ include/uapi/drm/amdgpu_drm.h
@@ -206,6 +206,7 @@ union drm_amdgpu_bo_list {
 #define AMDGPU_CTX_OP_FREE_CTX	2
 #define AMDGPU_CTX_OP_QUERY_STATE	3
 #define AMDGPU_CTX_OP_QUERY_STATE2	4
+#define AMDGPU_CTX_OP_SET_PSTATE_PROFILE	5
 
 /* GPU reset status */
 #define AMDGPU_CTX_NO_RESET		0
@@ -237,6 +238,14 @@ union drm_amdgpu_bo_list {
 */
 #define AMDGPU_CTX_PRIORITY_HIGH        512
 #define AMDGPU_CTX_PRIORITY_VERY_HIGH   1023
+
+/* select a profiling pstate for perfmon tools */
+#define AMDGPU_CTX_PSTATE_PROFILE_FLAGS_MASK  0xf
+#define AMDGPU_CTX_PSTATE_PROFILE_NONE  0
+#define AMDGPU_CTX_PSTATE_PROFILE_STANDARD  1
+#define AMDGPU_CTX_PSTATE_PROFILE_MIN_SCLK  2
+#define AMDGPU_CTX_PSTATE_PROFILE_MIN_MCLK  3
+#define AMDGPU_CTX_PSTATE_PROFILE_PEAK  4
 
 struct drm_amdgpu_ctx_in {
 	/** AMDGPU_CTX_OP_* */
