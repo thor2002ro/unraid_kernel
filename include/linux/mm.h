c588b376761c62fdba7fd88a1b1e6fe0a5353290
Gang Li <ligang.bdlg@bytedance.com>
2021-12-07 17:30:51 +1100

    vmscan: make drop_slab_node static
    drop_slab_node is only used in drop_slab.  So remove it's declaration from
    header file and add keyword static for it's definition.
    
    Link: https://lkml.kernel.org/r/20211111062445.5236-1-ligang.bdlg@bytedance.com
    Signed-off-by: Gang Li <ligang.bdlg@bytedance.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Muchun Song <songmuchun@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3167,7 +3167,6 @@ int drop_caches_sysctl_handler(struct ctl_table *, int, void *, size_t *,
 #endif
 
 void drop_slab(void);
-void drop_slab_node(int nid);
 
 #ifndef CONFIG_MMU
 #define randomize_va_space 0
