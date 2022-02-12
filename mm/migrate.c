5c00ad47b7b43cfe42d02114c54428cbc7190c41
Huang Ying <ying.huang@intel.com>
2022-02-12 16:38:31 +1100

    NUMA Balancing: add page promotion counter
    With the advent of various new memory types, some machines will have
    multiple types of memory, e.g.  DRAM and PMEM (persistent memory).  The
    memory subsystem of these machines can be called memory tiering system,
    because the performance of the different types of memory are different.
    
    After commit c221c0b0308f ("device-dax: "Hotplug" persistent memory for
    use like normal RAM"), the PMEM could be used as the cost-effective
    volatile memory in separate NUMA nodes.  In a typical memory tiering
    system, there are CPUs, DRAM and PMEM in each physical NUMA node.  The
    CPUs and the DRAM will be put in one logical node, while the PMEM will be
    put in another (faked) logical node.
    
    To optimize the system overall performance, the hot pages should be placed
    in DRAM node.  To do that, we need to identify the hot pages in the PMEM
    node and migrate them to DRAM node via NUMA migration.
    
    In the original NUMA balancing, there are already a set of existing
    mechanisms to identify the pages recently accessed by the CPUs in a node
    and migrate the pages to the node.  So we can reuse these mechanisms to
    build the mechanisms to optimize the page placement in the memory tiering
    system.  This is implemented in this patchset.
    
    At the other hand, the cold pages should be placed in PMEM node.  So, we
    also need to identify the cold pages in the DRAM node and migrate them to
    PMEM node.
    
    In commit 26aa2d199d6f ("mm/migrate: demote pages during reclaim"), a
    mechanism to demote the cold DRAM pages to PMEM node under memory pressure
    is implemented.  Based on that, the cold DRAM pages can be demoted to PMEM
    node proactively to free some memory space on DRAM node to accommodate the
    promoted hot PMEM pages.  This is implemented in this patchset too.
    
    We have tested the solution with the pmbench memory accessing benchmark
    with the 80:20 read/write ratio and the Gauss access address distribution
    on a 2 socket Intel server with Optane DC Persistent Memory Model.  The
    test results shows that the pmbench score can improve up to 95.9%.
    
    This patch (of 3):
    
    In a system with multiple memory types, e.g.  DRAM and PMEM, the CPU and
    DRAM in one socket will be put in one NUMA node as before, while the PMEM
    will be put in another NUMA node as described in the description of the
    commit c221c0b0308f ("device-dax: "Hotplug" persistent memory for use like
    normal RAM").  So, the NUMA balancing mechanism will identify all PMEM
    accesses as remote access and try to promote the PMEM pages to DRAM.
    
    To distinguish the number of the inter-type promoted pages from that of
    the inter-socket migrated pages.  A new vmstat count is added.  The
    counter is per-node (count in the target node).  So this can be used to
    identify promotion imbalance among the NUMA nodes.
    
    Link: https://lkml.kernel.org/r/20220128082751.593478-1-ying.huang@intel.com
    Link: https://lkml.kernel.org/r/20220128082751.593478-2-ying.huang@intel.com
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>
    Reviewed-by: Yang Shi <shy828301@gmail.com>
    Tested-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Cc: Wei Xu <weixugc@google.com>
    Cc: Shakeel Butt <shakeelb@google.com>
    Cc: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Feng Tang <feng.tang@intel.com>
    Cc: Hasan Al Maruf <hasanalmaruf@fb.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/migrate.c
+++ mm/migrate.c
@@ -2080,6 +2080,7 @@ int migrate_misplaced_page(struct page *page, struct vm_area_struct *vma,
 	pg_data_t *pgdat = NODE_DATA(node);
 	int isolated;
 	int nr_remaining;
+	int nr_succeeded;
 	LIST_HEAD(migratepages);
 	new_page_t *new;
 	bool compound;
@@ -2118,7 +2119,8 @@ int migrate_misplaced_page(struct page *page, struct vm_area_struct *vma,
 
 	list_add(&page->lru, &migratepages);
 	nr_remaining = migrate_pages(&migratepages, *new, NULL, node,
-				     MIGRATE_ASYNC, MR_NUMA_MISPLACED, NULL);
+				     MIGRATE_ASYNC, MR_NUMA_MISPLACED,
+				     &nr_succeeded);
 	if (nr_remaining) {
 		if (!list_empty(&migratepages)) {
 			list_del(&page->lru);
@@ -2127,8 +2129,13 @@ int migrate_misplaced_page(struct page *page, struct vm_area_struct *vma,
 			putback_lru_page(page);
 		}
 		isolated = 0;
-	} else
-		count_vm_numa_events(NUMA_PAGE_MIGRATE, nr_pages);
+	}
+	if (nr_succeeded) {
+		count_vm_numa_events(NUMA_PAGE_MIGRATE, nr_succeeded);
+		if (!node_is_toptier(page_to_nid(page)) && node_is_toptier(node))
+			mod_node_page_state(NODE_DATA(node), PGPROMOTE_SUCCESS,
+					    nr_succeeded);
+	}
 	BUG_ON(!list_empty(&migratepages));
 	return isolated;
 
