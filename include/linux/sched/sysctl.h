ebb891f03580b43412f24248f5cbd293bd2a7531
Zhen Ni <nizhen@uniontech.com>
2022-02-15 19:46:00 +0800

    sched: Move deadline_period sysctls to deadline.c
    move deadline_period sysctls to deadline.c and use the new
    register_sysctl_init() to register the sysctl interface.
    
    Signed-off-by: Zhen Ni <nizhen@uniontech.com>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -21,9 +21,6 @@ enum sched_tunable_scaling {
 	SCHED_TUNABLESCALING_END,
 };
 
-extern unsigned int sysctl_sched_dl_period_max;
-extern unsigned int sysctl_sched_dl_period_min;
-
 #ifdef CONFIG_UCLAMP_TASK
 extern unsigned int sysctl_sched_uclamp_util_min;
 extern unsigned int sysctl_sched_uclamp_util_max;
