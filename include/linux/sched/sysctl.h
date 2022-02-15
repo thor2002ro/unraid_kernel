5f6e55c2485c8df33e1e738fb952f5a2a721f432
Zhen Ni <nizhen@uniontech.com>
2022-02-15 19:45:59 +0800

    sched: Move rt_period/runtime sysctls to rt.c
    move rt_period/runtime sysctls to rt.c and use the new
    register_sysctl_init() to register the sysctl interface.
    
    Signed-off-by: Zhen Ni <nizhen@uniontech.com>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -21,15 +21,6 @@ enum sched_tunable_scaling {
 	SCHED_TUNABLESCALING_END,
 };
 
-/*
- *  control realtime throttling:
- *
- *  /proc/sys/kernel/sched_rt_period_us
- *  /proc/sys/kernel/sched_rt_runtime_us
- */
-extern unsigned int sysctl_sched_rt_period;
-extern int sysctl_sched_rt_runtime;
-
 extern unsigned int sysctl_sched_dl_period_max;
 extern unsigned int sysctl_sched_dl_period_min;
 
@@ -51,8 +42,6 @@ extern int sysctl_sched_rr_timeslice;
 extern int sched_rr_timeslice;
 
 int sched_rr_handler(struct ctl_table *table, int write, void *buffer,
-		size_t *lenp, loff_t *ppos);
-int sched_rt_handler(struct ctl_table *table, int write, void *buffer,
 		size_t *lenp, loff_t *ppos);
 int sysctl_sched_uclamp_handler(struct ctl_table *table, int write,
 		void *buffer, size_t *lenp, loff_t *ppos);
