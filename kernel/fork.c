bc84edb98cc372d37c1f0239dfeb0399fdda66f0
Dan Carpenter <dan.carpenter@oracle.com>
2022-03-17 11:51:46 +1100

    mm: remove unnecessary check in alloc_thread_stack_node()
    The "stack" pointer cannot be NULL at this point so there is no
    need to check.
    
    Link: https://lkml.kernel.org/r/20220301080706.GB17208@kili
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: Andrey Konovalov <andreyknvl@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- kernel/fork.c
+++ kernel/fork.c
@@ -328,8 +328,7 @@ static int alloc_thread_stack_node(struct task_struct *tsk, int node)
 	 * so cache the vm_struct.
 	 */
 	tsk->stack_vm_area = vm;
-	if (stack)
-		stack = kasan_reset_tag(stack);
+	stack = kasan_reset_tag(stack);
 	tsk->stack = stack;
 	return 0;
 }
