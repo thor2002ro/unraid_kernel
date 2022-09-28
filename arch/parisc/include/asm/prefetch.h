b0b334d9cced0de8a79b70c580416d202e9081ef
Helge Deller <deller@gmx.de>
2022-09-28 23:10:40 +0200

    parisc: Fix prefetch() and prefetchw() on 32-bit CPUs
    Main motivation for this patch is to allow to build a 32-bit kernel
    tuned for P8X00 CPUs while still being able to boot it on PA1.x
    machines.
    
    For that make sure that no 64-bit instructions are used on a 32-bit
    kernel and ignore accesses to page 0 due to the PA7300LC prefetch bug
    (see header of this header file).
    
    For a 64-bit build fall back to the built-in __builtin_prefetch()
    provided by include/linux/prefetch.h.
    
    Signed-off-by: Helge Deller <deller@gmx.de>
    Cc: stable@vger.kernel.org

--- arch/parisc/include/asm/prefetch.h
+++ arch/parisc/include/asm/prefetch.h
@@ -17,29 +17,19 @@
 #define __ASM_PARISC_PREFETCH_H
 
 #ifndef __ASSEMBLY__
-#ifdef CONFIG_PREFETCH
-
+#if defined(CONFIG_PREFETCH) && !defined(CONFIG_64BIT)
 #define ARCH_HAS_PREFETCH
+#define ARCH_HAS_PREFETCHW
+#define prefetchw	prefetch
 static inline void prefetch(const void *addr)
 {
 	__asm__(
-#ifndef CONFIG_PA20
 		/* Need to avoid prefetch of NULL on PA7300LC */
-		"	extrw,u,= %0,31,32,%%r0\n"
-#endif
+		"	extrw,u,= %0, 31-12, 32-12, %%r0\n"
 		"	ldw 0(%0), %%r0" : : "r" (addr));
 }
-
-/* LDD is a PA2.0 addition. */
-#ifdef CONFIG_PA20
-#define ARCH_HAS_PREFETCHW
-static inline void prefetchw(const void *addr)
-{
-	__asm__("ldd 0(%0), %%r0" : : "r" (addr));
-}
-#endif /* CONFIG_PA20 */
-
 #endif /* CONFIG_PREFETCH */
+
 #endif /* __ASSEMBLY__ */
 
 #endif /* __ASM_PARISC_PROCESSOR_H */
