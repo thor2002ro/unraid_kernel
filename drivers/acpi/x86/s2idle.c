f08fdf16f9742f728bf7175f7ea58ce8b28cc1f0
Mario Limonciello <mario.limonciello@amd.com>
2023-08-18 14:40:01 -0500

    Export-LPS0-constraints
    ACPI: Adjust #ifdef for *_lps0_dev use
    
    The `#ifdef` for acpi_register_lps0_dev() currently is guarded against
    `CONFIG_X86`, but actually the functions contained in the block are
    specifically sleep related functions.
    Adjust the guard to also check for `CONFIG_SUSPEND`.
    
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    
    ACPI: x86: s2idle: Catch multiple ACPI_TYPE_PACKAGE objects
    
    If a badly constructed firmware includes multiple `ACPI_TYPE_PACKAGE`
    objects while evaluating the AMD LPS0 _DSM, there will be a memory
    leak.  Explicitly guard against this.
    
    Suggested-by: Bjorn Helgaas <helgaas@kernel.org>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    
    ACPI: x86: s2idle: Add more debugging for AMD constraints parsing
    
    While parsing the constraints show all the entries for the table
    to aid with debugging other problems later.
    
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    
    ACPI: x86: s2idle: Add for_each_lpi_constraint() helper
    
    We have one existing and one coming user of this macro.
    Introduce a helper.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    
    ACPI: x86: s2idle: Add a function to get constraints for a device
    
    Other parts of the kernel may use constraints information to make
    decisions on what power state to put a device into.
    
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- drivers/acpi/x86/s2idle.c
+++ drivers/acpi/x86/s2idle.c
@@ -94,6 +94,11 @@ static struct lpi_constraints *lpi_constraints_table;
 static int lpi_constraints_table_size;
 static int rev_id;
 
+#define for_each_lpi_constraint(entry)						\
+	for (int i = 0;								\
+	     entry = &lpi_constraints_table[i], i < lpi_constraints_table_size;	\
+	     i++)
+
 static void lpi_device_get_constraints_amd(void)
 {
 	union acpi_object *out_obj;
@@ -113,6 +118,12 @@ static void lpi_device_get_constraints_amd(void)
 		union acpi_object *package = &out_obj->package.elements[i];
 
 		if (package->type == ACPI_TYPE_PACKAGE) {
+			if (lpi_constraints_table) {
+				acpi_handle_err(lps0_device_handle,
+						"Duplicate constraints list\n");
+				goto free_acpi_buffer;
+			}
+
 			lpi_constraints_table = kcalloc(package->package.count,
 							sizeof(*lpi_constraints_table),
 							GFP_KERNEL);
@@ -150,6 +161,13 @@ static void lpi_device_get_constraints_amd(void)
 					}
 				}
 
+				acpi_handle_debug(lps0_device_handle,
+						  "Name:%s, Enabled: %d, States: %d, MinDstate: %d\n",
+						  dev_info.name,
+						  dev_info.enabled,
+						  dev_info.function_states,
+						  dev_info.min_dstate);
+
 				if (!dev_info.enabled || !dev_info.name ||
 				    !dev_info.min_dstate)
 					continue;
@@ -157,9 +175,6 @@ static void lpi_device_get_constraints_amd(void)
 				status = acpi_get_handle(NULL, dev_info.name, &list->handle);
 				if (ACPI_FAILURE(status))
 					continue;
-
-				acpi_handle_debug(lps0_device_handle,
-						  "Name:%s\n", dev_info.name);
 
 				list->min_dstate = dev_info.min_dstate;
 
@@ -284,32 +299,52 @@ free_acpi_buffer:
 	ACPI_FREE(out_obj);
 }
 
+/**
+ * acpi_get_lps0_constraint - get the LPS0 constraint for a device
+ * @dev: device to get constraints for
+ *
+ * Returns:
+ *  - ACPI state value for constraint.
+ *  - Otherwise, ACPI_STATE_UNKNOWN.
+ */
+int acpi_get_lps0_constraint(struct acpi_device *adev)
+{
+	struct lpi_constraints *entry;
+
+	for_each_lpi_constraint(entry) {
+		if (adev->handle != entry->handle)
+			continue;
+		return entry->min_dstate;
+	}
+
+	return ACPI_STATE_UNKNOWN;
+}
+
 static void lpi_check_constraints(void)
 {
-	int i;
+	struct lpi_constraints *entry;
 
-	for (i = 0; i < lpi_constraints_table_size; ++i) {
-		acpi_handle handle = lpi_constraints_table[i].handle;
-		struct acpi_device *adev = acpi_fetch_acpi_dev(handle);
+	for_each_lpi_constraint(entry) {
+		struct acpi_device *adev = acpi_fetch_acpi_dev(entry->handle);
 
 		if (!adev)
 			continue;
 
-		acpi_handle_debug(handle,
+		acpi_handle_debug(entry->handle,
 			"LPI: required min power state:%s current power state:%s\n",
-			acpi_power_state_string(lpi_constraints_table[i].min_dstate),
+			acpi_power_state_string(entry->min_dstate),
 			acpi_power_state_string(adev->power.state));
 
 		if (!adev->flags.power_manageable) {
-			acpi_handle_info(handle, "LPI: Device not power manageable\n");
-			lpi_constraints_table[i].handle = NULL;
+			acpi_handle_info(entry->handle, "LPI: Device not power manageable\n");
+			entry->handle = NULL;
 			continue;
 		}
 
-		if (adev->power.state < lpi_constraints_table[i].min_dstate)
-			acpi_handle_info(handle,
+		if (adev->power.state < entry->min_dstate)
+			acpi_handle_info(entry->handle,
 				"LPI: Constraint not met; min power state:%s current power state:%s\n",
-				acpi_power_state_string(lpi_constraints_table[i].min_dstate),
+				acpi_power_state_string(entry->min_dstate),
 				acpi_power_state_string(adev->power.state));
 	}
 }
