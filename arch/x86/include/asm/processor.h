c60fb7e9cc77b911f9428da3fa420ae14a6ac784
Mario Limonciello <mario.limonciello@amd.com>
2024-10-03 16:37:59 -0500

    CPPC: Use heterogeneous core topology for identifying boost numerator
    AMD heterogeneous designs include two types of cores:
     * Performance
     * Efficiency
    
    Each core type has different highest performance values configured by the
    platform.  Drivers such as `amd_pstate` need to identify the type of
    core to correctly set an appropriate boost numerator to calculate the
    maximum frequency.
    
    X86_FEATURE_HETERO_CORE_TOPOLOGY is used to identify whether the SoC
    supports heterogeneous core type by reading CPUID leaf Fn_0x80000026.
    
    On performance cores the scaling factor of 196 is used.  On efficiency
    cores the scaling factor is the value reported as the highest perf.
    Efficiency cores have the same preferred core rankings.
    
    Suggested-by: Perry Yuan <perry.yuan@amd.com>
    Reviewed-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Link: https://lore.kernel.org/r/20241003213759.3038862-3-superm1@kernel.org
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -691,6 +691,14 @@ static inline u32 per_cpu_l2c_id(unsigned int cpu)
 	return per_cpu(cpu_info.topo.l2c_id, cpu);
 }
 
+/* defined by CPUID_Fn80000026_EBX BIT [31:28] */
+enum amd_core_type {
+	CPU_CORE_TYPE_NO_HETERO_SUP = -1,
+	CPU_CORE_TYPE_PERFORMANCE = 0,
+	CPU_CORE_TYPE_EFFICIENCY = 1,
+	CPU_CORE_TYPE_UNDEFINED = 2,
+};
+
 #ifdef CONFIG_CPU_SUP_AMD
 /*
  * Issue a DIV 0/1 insn to clear any division data from previous DIV
@@ -703,9 +711,14 @@ static __always_inline void amd_clear_divider(void)
 }
 
 extern void amd_check_microcode(void);
+extern enum amd_core_type amd_get_core_type(void);
 #else
 static inline void amd_clear_divider(void)		{ }
 static inline void amd_check_microcode(void)		{ }
+static inline enum amd_core_type amd_get_core_type(void)
+{
+	return CPU_CORE_TYPE_NO_HETERO_SUP;
+}
 #endif
 
 extern unsigned long arch_align_stack(unsigned long sp);
