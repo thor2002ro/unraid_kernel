6a9ba9c53f822cef977ee000bc8f51857ef3cb0a
thor2002ro <thor2002ro@yahoo.com>
2025-09-05 10:53:36 +0300

    [0/7] usb: xhci: Port Register Set improvements

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -1989,7 +1989,7 @@ static void handle_port_status(struct xhci_hcd *xhci, union xhci_trb *event)
 	hcd = port->rhub->hcd;
 	bus_state = &port->rhub->bus_state;
 	hcd_portnum = port->hcd_portnum;
-	portsc = readl(port->addr);
+	portsc = readl(&port->port_reg->portsc);
 
 	xhci_dbg(xhci, "Port change event, %d-%d, id %d, portsc: 0x%x\n",
 		 hcd->self.busnum, hcd_portnum + 1, port_id, portsc);
