1b63656720bca4e0712857fb6d620c48293f2b0a
thor2002ro <thor2002ro@yahoo.com>
2025-12-08 09:33:20 +0200

    drivers/pci: Allow attaching AER to non-RP devices that support MSI
    https://patchwork.kernel.org/project/linux-pci/patch/20251128122053.35909-1-darnshah@amazon.de/

--- drivers/pci/pcie/portdrv.c
+++ drivers/pci/pcie/portdrv.c
@@ -237,8 +237,8 @@ static int get_port_device_capability(struct pci_dev *dev)
 	}
 
 #ifdef CONFIG_PCIEAER
-	if ((pci_pcie_type(dev) == PCI_EXP_TYPE_ROOT_PORT ||
-             pci_pcie_type(dev) == PCI_EXP_TYPE_RC_EC) &&
+	if ((dev->msi_cap || pci_pcie_type(dev) == PCI_EXP_TYPE_ROOT_PORT ||
+	     pci_pcie_type(dev) == PCI_EXP_TYPE_RC_EC) &&
 	    dev->aer_cap && pci_aer_available() &&
 	    (pcie_ports_native || host->native_aer))
 		services |= PCIE_PORT_SERVICE_AER;
