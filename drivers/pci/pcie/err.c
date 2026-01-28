7159469f6391e3f591396446bc1b18870a906ef8
thor2002ro <thor2002ro@yahoo.com>
2026-01-28 10:46:10 +0200

    [v7,0/5] PCI/AER: Report fatal errors of RCiEP and EP if link recoverd
    https://patchwork.kernel.org/project/linux-pci/cover/20260124074557.73961-1-xueshuai@linux.alibaba.com/

--- drivers/pci/pcie/err.c
+++ drivers/pci/pcie/err.c
@@ -253,6 +253,11 @@ pci_ers_result_t pcie_do_recovery(struct pci_dev *dev,
 			pci_warn(bridge, "subordinate device reset failed\n");
 			goto failed;
 		}
+
+		/* Link recovered, report fatal errors of RCiEP or EP */
+		if (state == pci_channel_io_frozen)
+			aer_report_frozen_error(dev);
+
 	}
 
 	if (status == PCI_ERS_RESULT_NEED_RESET) {
@@ -280,7 +285,7 @@ pci_ers_result_t pcie_do_recovery(struct pci_dev *dev,
 	 */
 	if (host->native_aer || pcie_ports_native) {
 		pcie_clear_device_status(dev);
-		pci_aer_clear_nonfatal_status(dev);
+		pci_aer_raw_clear_status(dev);
 	}
 
 	pci_walk_bridge(bridge, pci_pm_runtime_put, NULL);
