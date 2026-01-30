afc29e73af1e30f060966bfd5503ec0de813f096
thor2002ro <thor2002ro@yahoo.com>
2026-01-30 09:08:19 +0200

    x86: remove the BIOS memory corruption check
    https://patchwork.kernel.org/project/linux-mm/patch/20260129220320.3012571-1-luizcap@redhat.com/

--- arch/x86/include/asm/bios_ebda.h
+++ arch/x86/include/asm/bios_ebda.h
@@ -20,21 +20,4 @@ static inline unsigned int get_bios_ebda(void)
 
 void reserve_bios_regions(void);
 
-#ifdef CONFIG_X86_CHECK_BIOS_CORRUPTION
-/*
- * This is obviously not a great place for this, but we want to be
- * able to scatter it around anywhere in the kernel.
- */
-void check_for_bios_corruption(void);
-void start_periodic_check_for_corruption(void);
-#else
-static inline void check_for_bios_corruption(void)
-{
-}
-
-static inline void start_periodic_check_for_corruption(void)
-{
-}
-#endif
-
 #endif /* _ASM_X86_BIOS_EBDA_H */
